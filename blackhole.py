5159f0894a5d14684ec7429dab9244cfd747177d
TJ Bearse <thomasb@qualtrics.com>
2017-06-26 16:32:22 -0700

    various changes

--- blackhole.py
+++ blackhole.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 #@app.route('/', methods=['GET', 'POST'], defaults={'path': ''})
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def black_hole(path):
-    print path
-    return ''
+    print request.get_data()
+    return '<html>stuff goes here</html>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=9090)
